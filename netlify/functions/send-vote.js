cd346d389417b46ef065f1ac87b44d0ec883a8bc
battama14 <169171805+battama14@users.noreply.github.com>
2025-06-06 15:50:23 +0200

    Update send-vote.js

--- netlify/functions/send-vote.js
+++ netlify/functions/send-vote.js
@@ -1,6 +1,29 @@
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '2004404',
+  key: '2adaefe3456db8023516',
+  secret: '872d62a9f173401fdbec',
+  cluster: 'eu',
+  useTLS: true
+});
+
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Fonction send-vote OK" }),
-  };
+  try {
+    const data = JSON.parse(event.body);
+    const { symbol, votes } = data;
+
+    await pusher.trigger('votes-channel', 'vote-event', { symbol, votes });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Vote envoyé avec succès' }),
+    };
+  } catch (error) {
+    console.error('Erreur fonction send-vote:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Erreur serveur' }),
+    };
+  }
 };
